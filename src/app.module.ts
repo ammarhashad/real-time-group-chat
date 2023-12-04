dc8a4f7b0c37144fa230c9de5512fcd7188e7e2c
Ammar <ammar.h.hashad@gmail.com>
2023-12-04 06:16:36 +0200

    configuring sequileze

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [UserModule, GroupModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
