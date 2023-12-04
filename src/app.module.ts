c6df6a7eae8e8fab03494ab1e4b55f6328542f3b
Ammar <ammar.h.hashad@gmail.com>
2023-12-04 06:16:08 +0200

    configuring sequileze

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, GroupModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.provider.ts
@@ -0,0 +1,21 @@
+
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'ammar',
+        password: 'ammarhashad',
+        database: 'nest',
+      });
+      sequelize.addModels([      ]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
